126510049154b7dfcfac7f808277448f63e4f2c6
ChrisR2487 <chrisroberts1109@gmail.com>
2023-07-24 17:02:04 -0400

    Reinstall dependecies

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,39 @@
+// Import necessary modules
 import React from 'react';
 import axios from 'axios';
 
 function App() {
+    // Define the function to start the server
     const startServer = async () => {
+        // Send a GET request to the /start route
         await axios.get('http://localhost:3000/start');
+
+        // Display an alert to the user
         alert('Server started');
     };
 
+    // Define the function to stop the server
     const stopServer = async () => {
+        // Send a GET request to the /stop route
         await axios.get('http://localhost:3000/stop');
+
+        // Display an alert to the user
         alert('Server stopped');
     };
 
+    // Render the component
     return (
         <div>
+            {/* When the "Start Server" button is clicked, call the startServer function */}
             <button onClick={startServer}>Start Server</button>
+
+            {/* When the "Stop Server" button is clicked, call the stopServer function */}
             <button onClick={stopServer}>Stop Server</button>
         </div>
     );
 }
 
+// Export the component
 export default App;
+
 
--- server.js
+++ server.js
@@ -1,25 +1,41 @@
+// Import necessary modules
 const express = require('express');
 const Docker = require('dockerode');
 const cors = require('cors');
 const docker = new Docker();
 
+// Create a new Express application
 const app = express();
 const port = 3000;
 
+// Use CORS middleware to allow cross-origin requests
 app.use(cors());
 
+// Define the /start route
 app.get('/start', async (req, res) => {
+    // Get the Docker container
     const container = docker.getContainer('minecraft-server');
+
+    // Start the container
     await container.start();
+
+    // Send a response back to the client
     res.send('Server started');
 });
 
+// Define the /stop route
 app.get('/stop', async (req, res) => {
+    // Get the Docker container
     const container = docker.getContainer('minecraft-server');
+
+    // Stop the container
     await container.stop();
+
+    // Send a response back to the client
     res.send('Server stopped');
 });
 
+// Start the server and listen for requests on the specified port
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
