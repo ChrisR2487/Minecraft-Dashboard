e4c869e5c304ed3fcfa206b74441cc1329aa882c
ChrisR2487 <chrisroberts1109@gmail.com>
2023-07-25 19:53:29 -0400

    Added log screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 // Import necessary modules
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
+import { io } from 'socket.io-client';
 
 function App() {
     // Define a new piece of state to keep track of the server status
     const [status, setStatus] = useState('Loading...');
 
-    // Use the useEffect hook to run code when the component mounts
+    // Define a new ref to keep track of the logs
+    const logsRef = useRef('');
+
+    // Use the useEffect hook to fetch the server status every 5 seconds
     useEffect(() => {
-        // Set up an interval to check the server status every 5 seconds
         const interval = setInterval(async () => {
             // Send a GET request to the /status route
             const response = await axios.get('http://localhost:3000/status');
@@ -19,6 +22,37 @@ function App() {
 
         // Clean up the interval when the component unmounts
         return () => clearInterval(interval);
+    }, []);
+
+    // Use the useEffect hook to connect to the Socket.IO server and fetch the logs
+     useEffect(() => {
+        // Connect to the Socket.IO server
+        const socket = io('http://localhost:3000');
+
+        // Send a 'getLogs' event to the server
+        socket.emit('getLogs');
+
+        socket.on('logs', (data) => {
+            console.log('Received logs:', data);
+            logsRef.current += data;
+        });
+
+        // Listen for 'logs' events from the server and append the received data to the logs
+        socket.on('logs', (data) => {
+            console.log('Received log data:', data);
+            logsRef.current += data;
+        });
+
+        socket.on('connect_error', (err) => {
+            console.log('Connection error:', err);
+        });
+
+        socket.on('reconnect_error', (err) => {
+            console.log('Reconnection error:', err);
+        });
+
+        // Disconnect from the server when the component unmounts
+        return () => socket.disconnect();
     }, []);
 
     // Define the function to start the server
@@ -44,11 +78,15 @@ function App() {
         <div>
             {/* Display the current server status */}
             <p>Server Status: {status}</p>
+
             {/* When the "Start Server" button is clicked, call the startServer function */}
             <button onClick={startServer}>Start Server</button>
 
             {/* When the "Stop Server" button is clicked, call the stopServer function */}
             <button onClick={stopServer}>Stop Server</button>
+
+            <h2>Logs</h2>
+            <textarea readOnly value={logsRef.current} style={{ width: '100%', height: '200px' }} />
         </div>
     );
 }
--- server.js
+++ server.js
@@ -2,19 +2,29 @@
 const express = require('express');
 const Docker = require('dockerode');
 const cors = require('cors');
-const docker = new Docker();
+const http = require('http');
+const { Server } = require('socket.io');
+const stream = require('stream');
 
-// Create a new Express application
+const docker = new Docker();
 const app = express();
+const server = http.createServer(app);
 const port = 3000;
 
-// Use CORS middleware to allow cross-origin requests
 app.use(cors());
+
+// Set up a new Socket.IO server with CORS enabled for the React app's origin
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3001',
+        methods: ['GET', 'POST']
+    }
+});
 
 // Define the /start route
 app.get('/start', async (req, res) => {
     // Get the Docker container
-    const container = docker.getContainer('1ac82c7fb55a');
+    const container = docker.getContainer('8cbc8b8cc245');
 
     // Start the container
     await container.start();
@@ -26,7 +36,7 @@ app.get('/start', async (req, res) => {
 // Define the /stop route
 app.get('/stop', async (req, res) => {
     // Get the Docker container
-    const container = docker.getContainer('1ac82c7fb55a');
+    const container = docker.getContainer('8cbc8b8cc245');
 
     // Stop the container
     await container.stop();
@@ -38,7 +48,7 @@ app.get('/stop', async (req, res) => {
 // Define the /status route
 app.get('/status', async (req, res) => {
     // Get the Docker container
-    const container = docker.getContainer('1ac82c7fb55a');
+    const container = docker.getContainer('8cbc8b8cc245');
 
     // Inspect the container to get its current state
     const data = await container.inspect();
@@ -47,8 +57,69 @@ app.get('/status', async (req, res) => {
     res.json({ status: data.State.Running ? "Running" : "Stopped" });
 });
 
+// Listen for connections from clients
+io.on('connection', (socket) => {
+    // Log when a user connects
+    console.log('a user connected');
+
+    // Listen for 'getLogs' events from clients
+    socket.on('getLogs', async () => {
+        console.log('Received getLogs event');
+
+        // Create a PassThrough stream to collect the logs
+        var logStream = new stream.PassThrough();
+
+        // Get the Docker container
+        const container = docker.getContainer('8cbc8b8cc245');
+
+        // Listen for data events from the log stream and send the data to the client
+        logStream.on('data', (chunk) => {
+            console.log('Received log data:', chunk.toString('utf8'));
+            socket.emit('logs', chunk.toString('utf8'));
+        });
+
+        // Fetch the logs from the container
+        container.logs({
+            follow: true,
+            stdout: true,
+            stderr: true,
+            timestamps: true
+        }, (err, logData) => {
+            if (err) {
+                console.error(err); // Log the error object to the console
+                // Send an error message to the client if fetching the logs fails
+                socket.emit('logs', 'An error occurred while fetching the logs.');
+                return;
+            }
+
+            // Demultiplex the log data into the log stream
+            container.modem.demuxStream(logData, logStream, logStream);
+            logData.on('end', () => {
+                logStream.end('!stop!');
+            });
+
+            // logStream.on('data', (chunk) => {
+            //     console.log('Log data:', chunk.toString('utf8'));
+            //     socket.emit('logs', chunk.toString('utf8'));
+            //     console.log('Sent logs');
+            // });
+
+        });
+    });
+
+    // Log when a user disconnects
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('error', (err) => {
+        console.log('Socket error:', err);
+    });
+
+});
+
 // Start the server and listen for requests on the specified port
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
 
