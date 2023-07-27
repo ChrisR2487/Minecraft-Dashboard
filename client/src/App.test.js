4ecc6abde1d31d3eed249132ea1c9e9e5052422c
ChrisR2487 <chrisroberts1109@gmail.com>
2023-07-27 17:07:13 -0400

    Added server mocking to client test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { setupServer } from 'msw/node';
-import { rest } from 'msw';
+import axios from 'axios';
 import App from './App';
 
-// Set up a mock server to intercept HTTP requests
-const server = setupServer(
-  rest.get('http://localhost:3000/status', (req, res, ctx) => {
-    return res(ctx.json({ status: 'Running' }));
-  }),
-  rest.get('http://localhost:3000/start', (req, res, ctx) => {
-    return res(ctx.text('Server started'));
-  }),
-  rest.get('http://localhost:3000/stop', (req, res, ctx) => {
-    return res(ctx.text('Server stopped'));
-  })
-);
+// Mock axios
+jest.mock('axios');
 
-beforeAll(() => server.listen());  // Start the mock server
-afterEach(() => server.resetHandlers());  // Reset any runtime request handlers
-afterAll(() => server.close());  // Stop the mock server
+// Define a helper function to mock a successful response from axios
+const mockAxiosGet = (url, response) => {
+  axios.get.mockImplementationOnce((url) => Promise.resolve(response));
+};
 
 test('starts and stops the server', async () => {
+  // Mock the responses for the status, start, and stop requests
+  mockAxiosGet('http://localhost:3000/status', { data: { status: 'Running' } });
+  mockAxiosGet('http://localhost:3000/stop', { data: 'Server stopped' });
+  mockAxiosGet('http://localhost:3000/start', { data: 'Server started' });
+
   render(<App />);
 
   // Check that the server status is displayed
@@ -35,4 +30,10 @@ test('starts and stops the server', async () => {
   // Click the "Start Server" button and check that the server is started
   userEvent.click(screen.getByText('Start Server'));
   await waitFor(() => screen.getByText('Status: Running'));
+
+  // Check that the correct axios.get calls were made
+  expect(axios.get).toHaveBeenCalledTimes(3);
+  expect(axios.get).toHaveBeenNthCalledWith(1, 'http://localhost:3000/status');
+  expect(axios.get).toHaveBeenNthCalledWith(2, 'http://localhost:3000/stop');
+  expect(axios.get).toHaveBeenNthCalledWith(3, 'http://localhost:3000/start');
 });
