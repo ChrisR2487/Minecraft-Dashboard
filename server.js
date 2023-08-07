1fb97fea92b21f52055126d8622d24832bdc7ec1
ChrisR2487 <chrisroberts1109@gmail.com>
2023-08-06 20:01:40 -0400

    Refactored to use compose for container operations

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const http = require('http');
 const { Server } = require('socket.io');
 const stream = require('stream');
+const compose = require('docker-compose');
+const path = require('path');
 
 const docker = new Docker();
 const app = express();
@@ -22,46 +24,95 @@ const io = new Server(server, {
 });
 
 // Define the /start route
-app.get('/start', async (req, res) => {
-    // Get the Docker container
-    const container = docker.getContainer('8cbc8b8cc245');
-
-    // Start the container
-    await container.start();
-
-    // Send a response back to the client
-    res.send('Server started');
+app.get('/start', (req, res) => {
+    compose.upAll({
+        cwd: path.dirname(require.main.filename),
+        log: true,
+        commandOptions: ['-d', '--force-recreate']
+    })
+    .then(() => {
+        console.log('Successfully started the service.');
+        res.status(200).send('Server started successfully.');
+    })
+    .catch(err => {
+        console.error('Failed to start the service.', err);
+        res.status(500).send(`Failed to start the server: ${err.message}`);
+    });
 });
 
 // Define the /stop route
-app.get('/stop', async (req, res) => {
-    // Get the Docker container
-    const container = docker.getContainer('8cbc8b8cc245');
-
-    // Stop the container
-    await container.stop();
-
-    // Send a response back to the client
-    res.send('Server stopped');
+app.get('/stop', (req, res) => {
+    compose.down({
+        cwd: path.dirname(require.main.filename),
+        log: true
+    })
+    .then(() => {
+        console.log('Successfully stopped the service.');
+        res.status(200).send('Server stopped successfully.');
+    })
+    .catch(err => {
+        console.error('Failed to stop the service.', err);
+        res.status(500).send(`Failed to stop the server: ${err.message}`);
+    });
 });
 
 // Define the /status route
 app.get('/status', async (req, res) => {
-    // Get the Docker container
-    const container = docker.getContainer('8cbc8b8cc245');
+    try {
+        // Get the status of all services
+        const servicesStatus = await compose.ps({ cwd: path.dirname(require.main.filename) });
 
-    // Inspect the container to get its current state
-    const data = await container.inspect();
+        // Split the output into lines
+        const lines = servicesStatus.out.split('\n');
 
-    // Send a response back to the client with the current status of the server
-    res.json({ status: data.State.Running ? "Running" : "Stopped" });
+        // Filter the lines to get the one corresponding to the minecraft service
+        const minecraftServiceLine = lines.find(line => line.includes('minecraft-server'));
+
+        if (minecraftServiceLine) {
+            // Split the line using two or more spaces as the delimiter
+            const columns = minecraftServiceLine.split(/ {2,}/);
+
+            // The status should be the column containing 'Up' or 'Exit'
+            const status = columns.find(col => col.startsWith('Up') || col.startsWith('Exit'));
+
+            // Return the status to the client
+            res.json({ status: status && status.startsWith('Up') ? 'Running' : 'Stopped' });
+        } else {
+            // If the service isn't found, indicate that it's not running
+            res.json({ status: 'Not Running' });
+        }
+    } catch (err) {
+        console.error('Error fetching service status:', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
+
+
+
 // Listen for connections from clients
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log('a user connected');
 
-    const container = docker.getContainer('8cbc8b8cc245');
+    // Retrieve the status of all services defined in docker-compose.yml
+    const servicesStatus = await compose.ps({
+        cwd: path.dirname(require.main.filename),
+        log: true
+    });
+
+    // Find the line in the output that corresponds to the 'minecraft-server' service
+    const minecraftServerService = servicesStatus.out.split('\n').find(line => line.includes('minecraft'));
+
+    // If the service isn't found in the output, emit an error message
+    if (!minecraftServerService) {
+        socket.emit('logs', 'Service not found.');
+        return;
+    }
+
+    // Extract the container ID from the output for the 'minecraft-server' service
+    const containerId = minecraftServerService.split(/\s+/)[0];
+
+    const container = docker.getContainer(containerId);
     const logStream = new stream.PassThrough();
 
     logStream.on('data', (chunk) => {
@@ -94,6 +145,7 @@ io.on('connection', (socket) => {
         console.log('Socket error:', err);
     });
 });
+
 
 // Start the server and listen for requests on the specified port
 server.listen(port, () => {
